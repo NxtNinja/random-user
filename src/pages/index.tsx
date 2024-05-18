9e7a941df236c85bb79f935ca9b84f0dbd75de5d
Priyangsu Banik <136341993+NxtNinja@users.noreply.github.com>
2024-05-18 19:24:08 +0530

    nextui setup done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import { Button, Tooltip } from "@nextui-org/react";
+
 const index = () => {
-  return <div>Enter</div>;
+  return (
+    <>
+      <div className="grid place-items-center h-dvh">
+        <Tooltip
+          showArrow={true}
+          color="warning"
+          content="Hover">
+          <Button
+            color="warning"
+            variant="ghost">
+            Click
+          </Button>
+        </Tooltip>
+      </div>
+    </>
+  );
 };
 
 export default index;
